d86d3a61454d30d77ddd49b38372102fdd544c37
Andrew Wilkinson <andywilkinson1993@gmail.com>
2019-04-03 13:55:37 +0100

    prog

--- chitchatter/resources/web/src/pages/HomePage.js
+++ chitchatter/resources/web/src/pages/HomePage.js
@@ -1,55 +1,32 @@
-import React, {useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import ChatContainer from "../components/ChatContainer";
 import ParticipantsContainer from "../components/ParticipantsContainer";
 import { Store } from "../store";
 
 const HomePage = () => {
-
   const { state } = useContext(Store);
 
   useEffect(() => {
     const socket = new WebSocket("ws://" + window.location.host + "/ws/" + state.channel);
 
     socket.onopen = () => {
-      console.log('Succesfully connected to chat server at ws://localhost:8080/ws/' + state.channel)
-  }
+      console.log("Succesfully connected to chat server at ws://localhost:8080/ws/" + state.channel);
+    };
 
-  socket.onclose = () => {
-      console.log('Goodbye!')
-  }
+    socket.onclose = () => {
+      console.log("Goodbye!");
+    };
 
-  socket.onerror = () => {
-      console.log('Uh oh...')
-  }
+    socket.onerror = () => {
+      console.log("Uh oh...");
+    };
 
-  socket.onmessage = event => {
-      const data = JSON.parse(event.data)
-      const { type } = data
-      switch (type) {
-          case 'message':
-              this.setState(state => ({
-                  messages: [...state.messages, { text: data.text, author: data.author }]
-              }))
-              this.scrollToBottom()
-              break
-          case 'joinChat':
-              this.setState(state => ({
-                  messages: data.previousMessages,
-                  author: data.author
-              }))
-              this.scrollToBottom()
-              break
-          case 'participantsUpdate':
-              this.setState(state => ({
-                  participants: data.participants
-              }))
-              break
-          default:
-              break
-      }
-    }
+    socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+      console.warn(event);
+      console.warn(data);
+    };
   }, [state.channel]);
-
 
   return (
     <React.Fragment>
