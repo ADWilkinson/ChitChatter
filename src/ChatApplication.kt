7af96b837b78333edc09f5b5b773bee78fff2ce6
Andy Wilkinson <andywilkinson1993@gmail.com>
2019-04-06 17:34:19 +0100

    test

--- src/ChatApplication.kt
+++ src/ChatApplication.kt
@@ -32,6 +32,11 @@ fun main() {
             ChatApplication().apply { main() }
         }
 
+        // Private API
+        connector {
+            host = "127.0.0.1"
+            port = 9090
+        }
         // Public API
         connector {
             port = System.getenv("PORT")?.toInt() ?: 8080
